e1d9cf34fe100ce88bd6dca876a78f8ab955ebc5
Achille <ishyarugemachille4@gmail.com>
2026-02-19 15:56:24 +0200

    Added palindrom.cpp
    This is a modular function that checks if a string is a palindrome or not.

---
+++ palindrome.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    string rev = s;
+    reverse(rev.begin(), rev.end());
+    cout << (s == rev ? "Yes" : "No");
+}
