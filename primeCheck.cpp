dbdf3220c6ee6865009badd959e9eae5cfe92084
Achille <ishyarugemachille4@gmail.com>
2026-02-19 15:57:23 +0200

    Added primeCheck.cpp
    A simple function to check if a number is prime or not in c++

---
+++ primeCheck.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    bool prime = true;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0) { prime = false; break; }
+    cout << (prime ? "Yes" : "No");
+}
